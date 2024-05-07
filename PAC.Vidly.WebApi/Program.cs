024ad96ac82c2ed171abc6410ac7eab85675703e
SoyLaw <127690307+SoyLaw@users.noreply.github.com>
2024-05-07 09:13:16 -0300

    Program - ConectionString y AddScoped

--- PAC.Vidly.WebApi/Program.cs
+++ PAC.Vidly.WebApi/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using PAC.Vidly.WebApi.DataAccess;
+using PAC.Vidly.WebApi.Services.Movies;
+using PAC.Vidly.WebApi.Services.Movies.Entities;
+using PAC.Vidly.WebApi.Services.Sessions;
+using PAC.Vidly.WebApi.Services.Sessions.Entities;
+using PAC.Vidly.WebApi.Services.Users;
+using PAC.Vidly.WebApi.Services.Users.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +20,23 @@ builder.Services
 
 var services = builder.Services;
 var configuration = builder.Configuration;
+
+var WebApiConnectionString = configuration.GetConnectionString("Vidly");
+if (string.IsNullOrEmpty(WebApiConnectionString))
+{
+    throw new Exception("Missing WebApi connection string");
+}
+
+services.AddDbContext<DbContext, PacVidlyDbContext>(options => options.UseSqlite(WebApiConnectionString));
+
+services.AddScoped<IRepository<User>, Repository<User>>();
+services.AddScoped<IUserService, UserService>();
+
+services.AddScoped<IRepository<Session>, Repository<Session>>();
+services.AddScoped<ISessionService, SessionService>();
+
+services.AddScoped<IRepository<Movie>, Repository<Movie>>();
+services.AddScoped<IMovieService, MovieService>();
 
 var app = builder.Build();
 
