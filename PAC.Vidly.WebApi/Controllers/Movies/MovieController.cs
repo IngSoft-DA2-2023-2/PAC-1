b7e380d8be87aa31bf9c980686ba950a2512722c
SoyLaw <127690307+SoyLaw@users.noreply.github.com>
2024-05-07 08:51:16 -0300

    MovieController - REFACTOR
    Se agrego filtro y uso de CreateMovieArgs

--- PAC.Vidly.WebApi/Controllers/Movies/MovieController.cs
+++ PAC.Vidly.WebApi/Controllers/Movies/MovieController.cs
@@ -1,32 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PAC.Vidly.WebApi.Controllers.Movies.Models;
+using PAC.Vidly.WebApi.Filters;
 using PAC.Vidly.WebApi.Services.Movies;
 using PAC.Vidly.WebApi.Services.Movies.Entities;
+using PAC.Vidly.WebApi.Services.Users;
+using PAC.Vidly.WebApi.Services.Users.Entities;
 
 namespace PAC.Vidly.WebApi.Controllers.Movies
 {
     [ApiController]
     [Route("")]
+    [AuthenticationFilter]
     public sealed class MovieController : ControllerBase
     {
         private readonly IMovieService _movieService;
+        private readonly IUserService _userService;
 
-        public MovieController(IMovieService movieService)
+        public MovieController(IMovieService movieService, IUserService userService)
         {
             _movieService = movieService;
+            _userService = userService;
         }
 
         [HttpPost]
-        public void Create(Movie? request)
+        public void Create(CreateMovieRequest? request)
         {
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
             }
 
-            var userLogged = GetUserLogged();
-
-            _movieService.Create(request, userLogged.Id);
+            User? userLogged = HttpContext.Items[Items.UserLogged] as User;
+            
+            var movieToSave = new CreateMovieArgs(request.Name);
+            
+            _movieService.Create(movieToSave, userLogged);
+            
         }
 
         [HttpGet]
